ed9aa3c729232414aa2aaa5b05b7c56c78d0fc91
Emil E. Gerlach <78322663+Lottithedeveloper@users.noreply.github.com>
2024-06-03 19:16:35 +0200

    Update app
    Adding bulletlist and more likes

--- c_p_demo/lib/widgets/likes_widget.dart
+++ c_p_demo/lib/widgets/likes_widget.dart
@@ -5,35 +5,40 @@ class LikesWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Center(
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 100,
-                ),
-                Text.rich(
-                  TextSpan(
-                    text: '\nJeg kan godt lide en masse ting!\n', // default text style
-                    children: <TextSpan>[
-                      TextSpan(text: '\nIs\n'),
-                      TextSpan(text: '\nKager\n'),
-                      TextSpan(text: '\nMad generelt\n'),
-                    ],
-                  ),
-                ),
-            Text.rich(
-              TextSpan(
-                text: '\nDer er også nogle ting jeg ikke kan lide!\n', // default text style
-                children: <TextSpan>[
-                  TextSpan(text: '\nBirketræer!\n'),
-                  TextSpan(text: '\nNår min kode ikke virker :(\n'),
-                ],
+    return Center(
+      child: Container(
+
+        margin: const EdgeInsets.all(15.0),
+
+        padding: const EdgeInsets.all(3.0),
+
+        height: 500,
+
+        child: const Expanded(
+
+          child: SingleChildScrollView(
+
+            scrollDirection: Axis.vertical,
+            child: Text(
+              "\nJeg kan godt lide en masse ting!\n"
+                  "\n Is\n"
+                  "\n Kager\n"
+                  "\n Mad generelt\n"
+                  "\n\n"
+                  "\nDer er også nogle ting jeg ikke kan lide!\n"
+                  "\n Birketræer!\n"
+                  "\n Når min kode ikke virker :(\n"
+                  "\n Vand i mine sko\n",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+                letterSpacing: 3,
+                wordSpacing: 3,
               ),
             ),
-              ]
-            )
-        )
+          ),
+        ),
+      ),
     );
   }
 }
